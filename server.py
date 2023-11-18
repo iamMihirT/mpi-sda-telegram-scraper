b02d4ca7587e7f2ce463ac860157f8108bc539a1
maany <imptodefeat@gmail.com>
2023-11-18 15:41:20 +0100

    fix server #2

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
-import subprocess
 import time
 import os
-import nest_asyncio
 from dotenv import load_dotenv
-from datetime import datetime
-from telegram_scraper import scrape
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List
+from datetime import datetime
 
+from app.sdk.models import BaseJob
 
 load_dotenv()
-nest_asyncio.apply()
 
 
 API_ID = os.getenv("API_ID")
@@ -18,13 +16,42 @@ API_HASH = os.getenv("API_HASH")
 PHONE = os.getenv("PHONE")
 USERNAME = os.getenv("USERNAME")
 HOST = os.getenv("HOST", "localhost")
-PORT = os.getenv("PORT", "8000")
+PORT = int(os.getenv("PORT", "8000"))
+MODE = os.getenv("MODE", "production")
+
+
+
+class CreateJobResponse(BaseJob):
+    output_lfn: List[str] = Field(descipton="Output LFNs")
+
 
 app = FastAPI()
+JOBS: List[BaseJob] = [BaseJob(id=0, name="test", output_lfn=["test"], input_lfns=["test"], status="created")]
+JOB_NONCE = 0
 
 class TelegramScrapeRequest(BaseModel):
     channel: str = "GCC_report"
     
+@app.get("/job")
+def list_all_jobs() -> List[BaseJob]:
+    return JOBS
+
+
+@app.post("/job")
+def create_job() -> BaseJob:
+    global JOB_NONCE
+    JOB_NONCE += 1  
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    name = f"telegram-{JOB_NONCE}-{date}"
+    output_lfns = [f"/telegram/{JOB_NONCE}/{date}/data2_climate.csv"]
+    job = BaseJob(
+        id=JOB_NONCE,
+        name=name,
+        output_lfn=output_lfns,
+    )
+    JOBS.append(job)
+    return job
+
 @app.put("/telegram/scrape")
 async def scrape_telegram(request: TelegramScrapeRequest):
     datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -33,11 +60,7 @@ async def scrape_telegram(request: TelegramScrapeRequest):
     print(f"{datetime} - Starting scraping {outfile}")
     start_time = time.time()
     # This should be a background task
-    await scrape(request.channel, outfile)
+    # await scrape(request.channel, outfile)
     print(f"{datetime} - Scraping completed in {time.time() - start_time} seconds")
     
     return {"data": "Scraping completed"}
-
-def server() -> None:
-    cmd = ["uvicorn", "main:app", "--reload", "--host", f"{HOST}", "--port", f"{PORT}"]
-    subprocess.call(cmd)
