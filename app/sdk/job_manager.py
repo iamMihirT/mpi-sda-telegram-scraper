8d6d3794ed925824527580c43e2e46a39d9f4b58
maany <imptodefeat@gmail.com>
2023-11-19 01:53:44 +0100

    add some sdk docs #2

--- app/sdk/job_manager.py
+++ app/sdk/job_manager.py
@@ -6,31 +6,35 @@ from app.sdk.models import BaseJob, TBaseJob
 
 class BaseJobManager(ABC, Generic[TBaseJob]):
     def __init__(self) -> None:
-        self._jobs: Dict[int, TBaseJob] = {} 
+        self._jobs: Dict[int, TBaseJob] = {}
         self._nonce = 0
-    
+
     @property
     def jobs(self) -> Dict[int, TBaseJob]:
         return self._jobs
-    
+
     @property
     def nonce(self) -> int:
         self._nonce = self._nonce + 1
         return self._nonce
-    
+
     @abstractmethod
     def make(self, *args: Any, **kwargs: Any) -> TBaseJob:
+        """
+        Return a new Job object. This function is called when a new job is
+        requested.
+        The parameters are decided by the subclass.
+        """
         raise NotImplementedError("make method must be implemented in a subclass.")
-
 
     def create_job(self, tracer_id: str, *args: Any, **kwargs: Any) -> BaseJob:
         id = self.nonce
         job = self.make(tracer_id=tracer_id, id=id, **kwargs)
-        self.jobs[job.id] = job # type: ignore
-        return job        
-    
+        self.jobs[job.id] = job  # type: ignore
+        return job
+
     def get_job(self, job_id: int) -> TBaseJob:
         return self.jobs[job_id]
-    
+
     def list_jobs(self) -> List[TBaseJob]:
         return list(self._jobs.values())
--- app/telegram_scraper_impl.py
+++ app/telegram_scraper_impl.py
@@ -1,21 +1,34 @@
 from typing import Any
 
-from pydantic import field_validator
 from app.sdk.job_manager import BaseJobManager
-from app.sdk.models import LFN, BaseJob, BaseJobState, Protocol, DataSource
+from app.sdk.models import BaseJob, BaseJobState
 from enum import Enum
 
+
 class TelegramScraperStage(Enum):
-    UPLOADING_TO_ES = "uploading_to_es"
+    """
+    Represents the stages of the Telegram Scraper job in addition to the
+    BaseJobState stages.
+    """
+
+    UPLOADING = "uploading"
+
 
 class TelegramScraperJob(BaseJob):
+    """
+    Represents a Telegram Scraper job.
+    """
+
     state: BaseJobState | TelegramScraperStage = BaseJobState.CREATED
+
 
 class TelegramScraperJobManager(BaseJobManager[TelegramScraperJob]):
     def __init__(self) -> None:
         super().__init__()
-    
-    def make(self, tracer_id: str, id: str, *args: Any, **kwargs: Any) -> TelegramScraperJob:
+
+    def make(
+        self, tracer_id: str, id: str, *args: Any, **kwargs: Any
+    ) -> TelegramScraperJob:
         job = TelegramScraperJob(
             id=int(id),
             tracer_id=tracer_id,
