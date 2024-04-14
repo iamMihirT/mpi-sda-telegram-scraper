624818102503b12ddee792c7cd022daf60bb13c6
maany <imptodefeat@gmail.com>
2024-04-14 22:38:10 +0200

    pass cli arguments

--- telegram_scraper.py
+++ telegram_scraper.py
@@ -15,10 +15,14 @@ def main(
     work_dir: str,
     kp_auth_token: str,
     kp_host: str,
+    kp_port: int,
+    kp_scheme: str,
     telegram_api_id: str,
     telegram_api_hash: str,
-    telegram_phone_number: str,
     openai_api_key: str,
+    telegram_phone_number: str | None = None,
+    telegram_password: str | None = None,
+    telegram_bot_token: str | None = None,
     log_level: str = "WARNING",
 ) -> None:
 
@@ -34,6 +38,8 @@ def main(
         logger=logger,
         kp_auth_token=kp_auth_token,
         kp_host=kp_host,
+        kp_port=kp_port,
+        kp_scheme=kp_scheme,
     )
 
     scraped_data_repository = ScrapedDataRepository(
@@ -48,6 +54,8 @@ def main(
         telegram_api_id=telegram_api_id,
         telegram_api_hash=telegram_api_hash,
         telegram_phone_number=telegram_phone_number,
+        telegram_password=telegram_password,
+        telegram_bot_token=telegram_bot_token,
     )
 
     import asyncio
@@ -106,6 +114,76 @@ if __name__ == "__main__":
 
     parser.add_argument("--work_dir", type=str, default="./.tmp", help="work dir")
 
+    parser.add_argument(
+        "--kp-auth-token",
+        type=str,
+        default="",
+        help="The Kernel Planckster auth token",
+    )
+
+    parser.add_argument(
+        "--kp-host",
+        type=str,
+        default="localhost",
+        help="The Kernel Planckster host",
+    )
+
+    parser.add_argument(
+        "--kp-port",
+        type=int,
+        default=8000,
+        help="The Kernel Planckster port",
+    )
+
+    parser.add_argument(
+        "--kp-scheme",
+        type=str,
+        default="http",
+        help="The Kernel Planckster scheme",
+    )
+
+    parser.add_argument(
+        "--telegram-api-id",
+        type=str,
+        default="",
+        help="The Telegram API ID",
+    )
+
+    parser.add_argument(
+        "--telegram-api-hash",
+        type=str,
+        default="",
+        help="The Telegram API Hash",
+    )
+
+    parser.add_argument(
+        "--telegram-phone-number",
+        type=str,
+        default="",
+        help="The Telegram phone number",
+    )
+
+    parser.add_argument(
+        "--telegram-password",
+        type=str,
+        default="",
+        help="The Telegram password",
+    )
+
+    parser.add_argument(
+        "--telegram-bot-token",
+        type=str,
+        default="",
+        help="The Telegram bot token",
+    )
+
+    parser.add_argument(
+        "--openai-api-key",
+        type=str,
+        default="",
+        help="The OpenAI API Key",
+    )
+
     args = parser.parse_args()
 
     main(
@@ -114,4 +192,14 @@ if __name__ == "__main__":
         tracer_id=args.tracer_id,
         work_dir=args.work_dir,
         log_level=args.log_level,
+        kp_auth_token=args.kp_auth_token,
+        kp_host=args.kp_host,
+        kp_port=args.kp_port,
+        kp_scheme=args.kp_scheme,
+        telegram_api_id=args.telegram_api_id,
+        telegram_api_hash=args.telegram_api_hash,
+        telegram_phone_number=args.telegram_phone_number,
+        telegram_password=args.telegram_password,
+        telegram_bot_token=args.telegram_bot_token,
+        openai_api_key=args.openai_api_key,
     )
